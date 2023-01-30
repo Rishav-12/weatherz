8a217dcd2937756aed0457db2dccaa249c078213
Rishav Mitra <rishavmitra10@gmail.com>
2023-01-30 09:05:38 +0000

    fix: use helmet middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening at ${port}`));
+app.use(helmet());
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
